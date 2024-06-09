85f22c9f83eb6337f6b29d47c1ac523fd14e8b87
arshad-shah <49516613+arshad-shah@users.noreply.github.com>
2024-06-09 14:08:07 +0000

    Build: (c3e5760) Initial work

--- src/components/Hero/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as MobileMenu } from './MobileMenu';
-export { default as Logo } from './Logo';
-export { default as DesktopMenu } from './DesktopMenu';
-export { default as MobileMenuButton } from './MobileMenuButton';
--- src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './Button';
-
-export * from './constants';
-
-export { default as IconButton } from './IconButton';
-
-export * from './Utils';
